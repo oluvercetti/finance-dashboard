6a12e430239f59f8e915bc92742b9df142a9b960
oluvercetti <ayawori25000@gmail.com>
2024-11-11 17:22:00 +0100

    Update forms/index.ts: Replace CustomInput with AuthCustomInput

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -1,15 +1,4 @@
 import CustomInput from "./CustomInput";
-import { z } from "zod";
+import AuthCustomInput from "./AuthCustomInput";
 
-export const authFormSchema = z.object({
-    email: z.string().email(),
-    password: z.string(),
-    firstName: z.string(),
-    lastName: z.string(),
-    address: z.string(),
-    state: z.string(),
-    postalCode: z.string(),
-    dateOfBirth: z.string(),
-    bvn: z.string().min(11, 'BVN must be 11 digits'),
-  })
-export { CustomInput };
+export { CustomInput, AuthCustomInput };
