42d2fccaad05b12a9d7763bc8621d5a1d87f5222
oluvercetti <ayawori25000@gmail.com>
2024-11-11 03:32:19 +0100

    Fix type declaration in AuthForm component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import AppLogo from './AppLogo'
 
-const AuthForm = ({ type }: { type: String }) => {
+const AuthForm = ({ type }: { type: string }) => {
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5">
