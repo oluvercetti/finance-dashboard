6b242354504b213a714cff4a1d235887fc72fea0
oluvercetti <ayawori25000@gmail.com>
2024-11-11 06:31:36 +0100

    Add UI components for forms and input fields

---
+++ components/uiComponents.ts
@@ -0,0 +1,11 @@
+export {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+export { Input } from "@/components/ui/input";
