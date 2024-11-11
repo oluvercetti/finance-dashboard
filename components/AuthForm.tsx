20ac3e437ceae00f503ce5030cddefc58794fd7f
oluvercetti <ayawori25000@gmail.com>
2024-11-11 03:29:41 +0100

    Add AuthForm component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import AppLogo from './AppLogo'
+
+const AuthForm = ({ type }: { type: String }) => {
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5">
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
+          <AppLogo />
+        </Link>
+      </header>
+    </section>
+  )
+}
+
+export default AuthForm
