833c70bbbd35abd39e62ba6b59b5668c0b1cba6a
oluvercetti <ayawori25000@gmail.com>
2024-11-12 18:27:26 +0100

    Add password validation and city field to authFormSchema

--- lib/models.ts
+++ lib/models.ts
@@ -3,10 +3,11 @@ import { z } from "zod";
 
 export const authFormSchema = (isSignIn: boolean) => z.object({
     email: z.string().email(),
-    password: z.string(),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
     firstName: isSignIn ? z.string().optional() : z.string(),
     lastName: isSignIn ? z.string().optional() : z.string(),
     address: isSignIn ? z.string().optional() : z.string(),
+    city: isSignIn ? z.string().optional() : z.string(),
     state: isSignIn ? z.string().optional() : z.string(),
     postalCode: isSignIn ? z.string().optional() : z.string(),
     dateOfBirth: isSignIn ? z.string().optional() : z.string(),
