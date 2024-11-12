f1776d620cfea1db898dc3dc87d43ed89472f73d
oluvercetti <ayawori25000@gmail.com>
2024-11-12 21:12:51 +0100

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,35 @@
+import { logoutAccount } from '@/lib/actions/user.actions'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const Footer = ({ user, type = "full" }: SidebarProps) => {
+    const isFull = type === "full"
+    const router = useRouter()
+    const handleLogout = async () => {
+        const loggedOut = await logoutAccount();
+        if (loggedOut) router.push('/sign-in')
+    }
+    return (
+        <footer className='footer'>
+            <div className={isFull ? "footer_name" : "footer_name-mobile"}>
+                <p className="text-xl font-bold text-gray-700">
+                    {user?.name[0]}
+                </p>
+            </div>
+            <div className={isFull ? "footer_email" : "footer_email-mobile"}>
+                <h1 className='text-14 truncate text-gray-700 font-semibold'>
+                    {user?.name}
+                </h1>
+                <p className="text-14 truncate font-normal text-gray-600">
+                    {user?.email}
+                </p>
+            </div>
+            <div className="footer_image" onClick={handleLogout}>
+                <Image src="icons/logout.svg" fill alt='logout'/>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
