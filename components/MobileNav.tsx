bd585255c7190559c3d9577abf3eefa9007a9713
oluvercetti <ayawori25000@gmail.com>
2024-11-13 06:02:00 +0100

    Add Footer component to MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import Image from "next/image"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import AppLogo from "./AppLogo";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname()
@@ -46,7 +47,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                 USER
                             </nav>
                         </SheetClose>
-                        FOOTER
+                        <Footer user={user} type="compact"/>
                     </div>
                 </SheetContent>
             </Sheet>
