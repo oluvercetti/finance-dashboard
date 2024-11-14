373ff67eb84faa1e85f9ec5632e2e95cae53672b
oluvercetti <ayawori25000@gmail.com>
2024-11-14 06:27:24 +0100

    Update SignUpParams and NewDwollaCustomerParams in types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,14 +8,14 @@ declare type SearchParamProps = {
 // ========================================
 
 declare type SignUpParams = {
-  firstName?: string;
-  lastName?: string;
-  address?: string;
-  city?: string;
-  state?: string;
-  postalCode?: string;
-  dateOfBirth?: string;
-  bvn?: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  bvn: string;
   email: string;
   password: string;
 };
@@ -129,12 +129,12 @@ declare type NewDwollaCustomerParams = {
   lastName: string;
   email: string;
   type: string;
-  address1: string;
+  address: string;
   city: string;
   state: string;
   postalCode: string;
   dateOfBirth: string;
-  ssn: string;
+  bvn: string;
 };
 
 declare interface CreditCardProps {
