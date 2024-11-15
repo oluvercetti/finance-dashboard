8cb9a6b91198bf8d009ab36633fb649cda2050f8
oluvercetti <ayawori25000@gmail.com>
2024-11-15 12:42:29 +0100

    Refactor Home component to fetch and display account data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,45 @@
 import HeaderBox from "@/components/HeaderBox"
 import RightSidebar from "@/components/RightSidebar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions"
 import { getLoggedInUser } from "@/lib/actions/user.actions"
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser()
-    return (
+
+  const accounts = await getAccounts({ userId: loggedIn?.$id })
+
+  if (!accounts) return;
+  const accountsData = accounts?.data;
+  
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
+  return (
     <section className='home'>
       <div className="home-content">
         <header className="home-header">
           <HeaderBox
             type='greeting'
             title='Welcome'
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.firstName || 'Guest'}
             subtext='Horizon is a modern banking platform for the future.'
           />
 
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1230.54}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
 
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar 
-      user={loggedIn}
-      transactions={[]}
-      banks={[]}
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   )
