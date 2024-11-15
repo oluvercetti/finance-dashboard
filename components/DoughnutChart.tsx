735a42aaaa322501b834a59f0c81c19973e16ebd
oluvercetti <ayawori25000@gmail.com>
2024-11-15 12:42:38 +0100

    Refactor DoughnutChart to use dynamic data

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,15 +6,19 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+
+  const bankNames = accounts?.map(account => account?.name);
+  const bankBalances = accounts?.map(account => account?.currentBalance);
+
   const data = {
     datasets: [
       {
         label: "Bank",
-        data: [1000, 2000, 3000],
+        data: bankBalances,
         backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
       },
     ],
-    labels: ["GTbank", "Access Bank", "Zenith Bank"],
+    labels: bankNames,
   }
   return (
     <Doughnut 
