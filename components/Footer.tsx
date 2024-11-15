828ae63568d8b2544f18cef75532ec6276764d05
oluvercetti <ayawori25000@gmail.com>
2024-11-15 12:42:46 +0100

    Update user name and email in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,12 +16,12 @@ const Footer = ({ user, type = "full" }: SidebarProps) => {
         <footer className='footer'>
             <div className={isFull ? "footer_name" : "footer_name-mobile"}>
                 <p className="text-xl font-bold text-gray-700">
-                    {user?.name[0]}
+                    {user?.firstName[0]}
                 </p>
             </div>
             <div className={isFull ? "footer_email" : "footer_email-mobile"}>
                 <h1 className='text-14 truncate text-gray-700 font-semibold'>
-                    {user?.name}
+                    {user?.firstName} {user?.lastName}
                 </h1>
                 <p className="text-14 truncate font-normal text-gray-600">
                     {user?.email}
