121d244f1b71aaf3fa06c621ac430e1003009d18
oluvercetti <ayawori25000@gmail.com>
2024-11-15 20:45:43 +0100

    Add RecentTransactions component to Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox"
+import RecentTransactions from "@/components/RecentTransactions"
 import RightSidebar from "@/components/RightSidebar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions"
@@ -12,9 +13,11 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   if (!accounts) return;
   const accountsData = accounts?.data;
   
+  const currentPage = Number(page as string) || 1;
+
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({ appwriteItemId });
+  const singleAccount = await getAccount({ appwriteItemId });
 
   return (
     <section className='home'>
@@ -34,7 +37,12 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
           />
         </header>
 
-        RECENT TRANSACTIONS
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={singleAccount?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
       <RightSidebar
         user={loggedIn}
