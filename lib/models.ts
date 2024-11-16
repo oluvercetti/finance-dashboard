f6fa964be67c44121d98248f3ed6c0e33165ab10
oluvercetti <ayawori25000@gmail.com>
2024-11-16 08:01:38 +0100

    Add paymentFormSchema to models.ts

--- lib/models.ts
+++ lib/models.ts
@@ -13,3 +13,11 @@ export const authFormSchema = (isSignIn: boolean) => z.object({
     dateOfBirth: isSignIn ? z.string().optional() : z.string(),
     ssn: isSignIn ? z.string().optional() : z.string(),
   })
+
+export const paymentFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  name: z.string().min(4, "Transfer note is too short"),
+  amount: z.string().min(4, "Amount is too short"),
+  senderBank: z.string().min(4, "Please select a valid bank account"),
+  sharableId: z.string().min(8, "Please select a valid sharable Id"),
+});
