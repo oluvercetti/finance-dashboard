c2a7b4cbd930bb6ce392bc0c206957654ac1abc8
oluvercetti <ayawori25000@gmail.com>
2024-11-16 04:08:44 +0100

    Add category styles to CategoryBadge component

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -7,16 +7,20 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/uiComponents"
+import { transactionCategoryStyles } from "@/constants"
 import { cn, formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils"
 
 const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+    const { borderColor, backgroundColor, textColor, chipBackgroundColor } = transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles.default;
+
     return (
-        <div className={cn('category-badge')}>
-            <div className={cn('size-2 rounded-full')} />
-            <p className={cn('text-[12px] font-medium')}>{category}</p>
+        <div className={cn('category-badge', borderColor, chipBackgroundColor)}>
+            <div className={cn('size-2 rounded-full', backgroundColor)} />
+            <p className={cn('text-[12px] font-medium', textColor)}>{category}</p>
         </div>
     )
 }
+
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
     return (
         <Table>
