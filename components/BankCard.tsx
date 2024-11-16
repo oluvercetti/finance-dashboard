a795d277be40a746a77b44857582e528871257bb
oluvercetti <ayawori25000@gmail.com>
2024-11-16 08:00:54 +0100

    Update BankCard component to include Copy component

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,11 +2,12 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
     return (
         <div className="flex flex-col">
-            <Link href="/" className="bank-card">
+            <Link href={`/transaction-history/?id=${account?.appwriteItemId}`} className="bank-card">
                 <div className="bank-card_content">
                     <div>
                         <h1 className="text-16 font-semibold text-white">
@@ -26,7 +27,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                             </h2>
                         </div>
                         <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                        ●●●● ●●●● ●●●● <span className="text-16">
+                            ●●●● ●●●● ●●●● <span className="text-16">
                                 {account.mask ?? 1234}
                             </span>
                         </p>
@@ -35,10 +36,11 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
 
                 <div className="bank-card_icon">
                     <Image src="/icons/Paypass.svg" alt="pay" width={20} height={24} />
-                    <Image src="/icons/mastercard.svg" alt="mastercard" width={45} height={32} className='ml-5'/>
+                    <Image src="/icons/mastercard.svg" alt="mastercard" width={45} height={32} className='ml-5' />
                 </div>
                 <Image src="/icons/lines.png" alt="lines" width={316} height={190} className="absolute top-0 left-0" />
             </Link>
+            {showBalance && <Copy title={account?.sharableId} />}
         </div>
     )
 }
