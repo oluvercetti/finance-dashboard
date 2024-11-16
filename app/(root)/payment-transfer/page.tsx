e9b245c4d2fe0edef100c1b75b6e82872343a1a7
oluvercetti <ayawori25000@gmail.com>
2024-11-16 08:00:42 +0100

    Add PaymentTransferForm and HeaderBox components to PaymentTransfer page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,15 @@
+import HeaderBox from '@/components/HeaderBox'
+import PaymentTransferForm from '@/components/PaymentTransferForm'
 import React from 'react'
 
 const PaymentTransfer = () => {
   return (
-    <div>PaymentTransfer</div>
+    <section className="payment-transfer">
+      <HeaderBox title="Payment Transfer" subtext="Kindly provide detials to make a transfer"/>
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={[]}/>
+      </section>
+    </section>
   )
 }
 
