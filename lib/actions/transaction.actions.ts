53e181d95e4fd20300101ffa2f8b8153f6544fa8
oluvercetti <ayawori25000@gmail.com>
2024-11-16 08:01:52 +0100

    Add getTransactionsByBankId and createTransaction functions

--- lib/actions/transaction.actions.ts
+++ lib/actions/transaction.actions.ts
@@ -3,3 +3,7 @@
 export const getTransactionsByBankId = async ({ bankId }: getTransactionsByBankIdProps) => {
     return {documents: []};
 }
+
+export const createTransaction = async ({ bankId, amount, type, description }: any) => {
+    return {documents: []};
+}
