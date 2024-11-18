4cab8fab9d09f77131edb2e270649b976160543e
oluvercetti <ayawori25000@gmail.com>
2024-11-18 13:29:08 +0100

    Add AuthContext component

---
+++ providers/AuthContext.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from 'react';
+
+// Define the initial state of your context
+interface MyContextProps {
+    children: React.ReactNode;
+}
+
+interface AuthContextProps {
+    loggedInUser: any;
+    setLoggedInUser: (user: any) => void;
+    accounts: any;
+    setAccounts: (accounts: any) => void;
+}
+
+// Create a new context
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+// Create a provider component
+export const AuthContextProvider = ({ children }: MyContextProps) => {
+    // Define your state and any necessary functions
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    const [accounts, setAccounts] = useState(null);
+
+
+    // Add any functions to update the state
+    const contextValue = {
+        loggedInUser,
+        setLoggedInUser,
+        accounts,
+        setAccounts,
+    }
+    // Return the provider component with the state and functions
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuthContext = () => {
+    const Context = useContext(AuthContext);
+
+    if (!Context) {
+        throw Error('Component needs to be a descendant of AuthContextProvider');
+    }
+
+    return Context;
+};
