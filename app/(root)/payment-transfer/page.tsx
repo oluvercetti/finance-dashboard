b0da12201095cd7a4e840f61ec11113670c6febf
oluvercetti <ayawori25000@gmail.com>
2024-11-18 08:30:03 +0100

    Add async/await to PaymentTransfer function and fetch accounts data

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,13 +1,20 @@
 import HeaderBox from '@/components/HeaderBox'
 import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const PaymentTransfer = () => {
+const PaymentTransfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if (!accounts) return;
+  const accountsData = accounts?.data;
   return (
     <section className="payment-transfer">
-      <HeaderBox title="Payment Transfer" subtext="Kindly provide detials to make a transfer"/>
+      <HeaderBox title="Payment Transfer" subtext="Kindly provide detials to make a transfer" />
       <section className="size-full pt-5">
-        <PaymentTransferForm accounts={[]}/>
+        <PaymentTransferForm accounts={accountsData} />
       </section>
     </section>
   )
