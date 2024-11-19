f908c29ee44301be179adb1aae6be9d8c2ff9bea
oluvercetti <ayawori25000@gmail.com>
2024-11-19 16:40:41 +0100

    Update FullPageLoader background color

--- components/FullPageLoader.tsx
+++ components/FullPageLoader.tsx
@@ -7,7 +7,7 @@ interface FullPageLoaderProps {
 
 const FullPageLoader = ({ text = "Loading...", type = "Spinner" }: FullPageLoaderProps) => {
   return (
-    <div className="flex items-center justify-center bg-white z-50 w-full mt-[15%]">
+    <div className="flex items-center justify-center bg-transparent z-50 w-full mt-[15%]">
       {type === "Spinner" && <div className="loader2" />}
       {type === "Loader" && <div className="loader" />}
       <p className="animate-pulse text-blue-500 text-lg ml-4">{text}</p>
