af5b7c193b3432801514f2f023fc0f88303d042e
oluvercetti <ayawori25000@gmail.com>
2024-11-19 16:11:42 +0100

    Add AuthContextProvider to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import SideBar from "@/components/SideBar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { AuthContextProvider } from "@/providers/AuthContext";
 
 export default async function RootLayout({
   children,
@@ -10,22 +11,24 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = await getLoggedInUser().catch((error) => {
-    redirect("/sign-in") 
+    redirect("/sign-in")
   })
   if (!loggedIn) redirect("/sign-in");
 
   return (
-    <main className="flex h-screen w-full font-inter">
-      <SideBar user={loggedIn} />
-      <div className="flex size-full flex-col">
-        <div className="root-layout">
-          <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30} />
-          <div>
-            <MobileNav user={loggedIn} />
+    <AuthContextProvider>
+      <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedIn} />
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30} />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
           </div>
+          {children}
         </div>
-        {children}
-      </div>
-    </main>
+      </main>
+    </AuthContextProvider>
   );
 }
